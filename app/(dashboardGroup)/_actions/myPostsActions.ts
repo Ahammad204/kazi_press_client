b35421a88e548ee4c9dc3d3b5aa152bf6dcae076
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-07-31 21:54:02 +0600

    feat: enhance post creation and update functionality with improved revalidation and error handling

--- app/(dashboardGroup)/_actions/myPostsActions.ts
+++ app/(dashboardGroup)/_actions/myPostsActions.ts
@@ -37,13 +37,67 @@ export const createPost = async (prevState: PostState, formData: FormData) => {
   });
   const result = await res.json();
   if (result.success) {
-    revalidateTag("my-posts", "max");
+    revalidateTag("my-posts", {
+      expire: 0,
+    });
   }
 
   if (result.success && result.data.IsPremium) {
-    revalidateTag("premium-posts", "max");
+    revalidateTag("premium-posts", {
+      expire: 0,
+    });
   } else {
-    revalidateTag("public-posts", "max");
+    revalidateTag("public-posts", {
+      expire: 0,
+    });
+  }
+
+  return result;
+};
+export const updatePost = async (
+  postId: string,
+  prevState: PostState,
+  formData: FormData,
+) => {
+  const payload = {
+    title: formData.get("title") ?? "",
+    content: formData.get("content") ?? "",
+    thumbnail: formData.get("thumbnail") ?? "",
+    tags: (formData.get("tags") as string).split(", ") ?? "",
+    IsPremium: formData.get("isPremium") === "on",
+  };
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value;
+  if (!accessToken) {
+    // throw new Error("User not logged in");
+    return {
+      success: false,
+      message: "User not logged in",
+    };
+  }
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/posts/${postId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify(payload),
+  });
+  const result = await res.json();
+  if (result.success) {
+    revalidateTag("my-posts", {
+      expire: 0,
+    });
+  }
+
+  if (result.success && result.data.IsPremium) {
+    revalidateTag("premium-posts", {
+      expire: 0,
+    });
+  } else {
+    revalidateTag("public-posts", {
+      expire: 0,
+    });
   }
 
   return result;
