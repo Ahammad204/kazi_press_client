939a79fe217f5f3c02ddcadaa4513cce39596031
Kazi Ahammad Ullah <ahammadcode@gmail.com>
2026-08-01 08:30:36 +0600

    feat: implement dashboard sidebar with role-based navigation items
    - Added DashboardSidebar component to manage sidebar navigation based on user roles (USER, AUTHOR, ADMIN).
    - Created sidebar menu items configuration for each user role in separate files (adminSidebarItems.ts, authorSidebarItems.ts, sidebarMenuItems.ts).
    - Integrated SidebarProvider to manage sidebar state and responsiveness.
    - Developed reusable UI components for sidebar, including Sidebar, SidebarMenu, and SidebarMenuItem.
    - Introduced utility hooks and types for better type safety and mobile responsiveness.
    - Added Skeleton and Tooltip components for enhanced UI experience.

--- app/(dashboardGroup)/layout.tsx
+++ app/(dashboardGroup)/layout.tsx
@@ -1,6 +1,9 @@
 import { Navbar } from "@/components/shared/navbar";
+import { SidebarProvider } from "@/components/ui/sidebar";
 import { getMe } from "@/service/getMe";
 import React from "react";
+import DashboardSidebar from "./_components/DashboardSidebar";
+
 
 const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
 
@@ -8,7 +11,12 @@ const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <Navbar user={user}></Navbar>
-      {children}
+         <SidebarProvider>
+        <div className="flex flex-1">
+          <DashboardSidebar user={user}  />
+          <main className="flex-1 min-w-0">{children}</main>
+        </div>
+      </SidebarProvider>
     </div>
   );
 };
